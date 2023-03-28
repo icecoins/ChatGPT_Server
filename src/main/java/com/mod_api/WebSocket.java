375088aba52db9638bde620514afbe9d2244ef61
BigNoseDewdrop <woyaogtmdttkp@gmail.com>
2023-03-28 11:09:19 +0800

    更新了通讯标记 新增调用vits模型接口进行语音合成的功能（并未完善）

--- src/main/java/com/mod_api/WebSocket.java
+++ src/main/java/com/mod_api/WebSocket.java
@@ -5,12 +5,14 @@ import com.config.MySpringConfigurator;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.*;
@@ -21,8 +23,19 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @Slf4j
 @ServerEndpoint(value = "/webSocket/{userId}", configurator = MySpringConfigurator.class)
 public class WebSocket {
-    private String marked_key = "marked_key"
-            ,preset_api_key = "preset_api_key";
+    private final String MARKED_KEY = "PicACG.2.2.1.3.3.4";
+    private final String PRESET_KEY = "sk-8g***4bpab***6CqJ***3Blb***V1un***LuL***sk4nruf";
+
+    // 分别为 对话发送结束标记、文件发送结束标记、文件错误标记，可自行设置，与APP代码中的标记一致即可
+    private final String SEND_END = "///**END_OF_SEND**///";
+    private final String FILE_END = "///**END_OF_FILE**///";
+    private final String FILE_ERROR = "///**FILE_ERROR**///";
+    private final String GPT_3_5_URL = "https://api.openai.com/v1/chat/completions";
+    private final String GPT_3_URL = "https://api.openai.com/v1/completions";
+
+    // 调用vits模型进行语音合成的接口地址，根据你的python代码自行设置
+    // 本人调用的vits接口返回值为 .mp3文件绝对路径 ，其他返回形式请自行修改代码
+    private final String LOCAL_AUDIO_API_URL = "http://127.0.0.1:65432/getAudio";
     private Session session;
     private String userId;
     private static CopyOnWriteArraySet<WebSocket> webSockets =new CopyOnWriteArraySet<>();
@@ -34,7 +47,8 @@ public class WebSocket {
             this.userId = userId;
             webSockets.add(this);
             sessionPool.put(userId, session);
-            System.out.println("New: "+ userId + "\nCount:"+webSockets.size());
+            System.out.println(getDate().getString("time") + "  New: "+
+                    userId + "\nCount:"+webSockets.size());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,27 +59,34 @@ public class WebSocket {
         try {
             webSockets.remove(this);
             sessionPool.remove(this.userId);
-            System.out.println("Close: " + userId + "\nCount:"+webSockets.size());
+            System.out.println(getDate().getString("time") + "  Close: " +
+                    userId + "\nCount:"+webSockets.size());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    @OnMessage
+    @OnMessage(maxMessageSize =  1048576)
     public void onMessage(String message) {
         new Thread(()->{
             JSONObject jsonObject = JSONObject.parseObject(message);
+            if (!(null == jsonObject.getString("type")) &&
+                    jsonObject.getString("type").equals("sound")) {
+                callTtsApi(jsonObject);
+                return;
+            }
             String api_key = jsonObject.getString("api_key");
-            System.out.println("user: " + userId + "\nKEY :" + api_key + "\nmsg: " + jsonObject);
+            System.out.println(getDate().getString("time") + "\nuser: " +
+                    userId + "\nKEY :" + api_key + "\nmsg: " + jsonObject);
             // 当传入的key为自定义的特殊标记时，用预设的key替换，以达到多个APP端共用一个key的效果
-            if(api_key.equals(marked_key)){
-                api_key = preset_api_key;
+            if(api_key.equals(MARKED_KEY)){
+                api_key = PRESET_KEY;
             }
             jsonObject.remove("api_key");
             if(jsonObject.getString("model").equals("gpt-3.5-turbo") ||
                     jsonObject.getString("model").equals("gpt-3.5-turbo-0301") ){
                 JSONObject msg = new JSONObject();
-                List<JSONObject>list = new ArrayList<>();
+                List<JSONObject> list = new ArrayList<>();
                 msg.put("role", "user");
                 msg.put("content", jsonObject.getString("prompt"));
                 list.add(msg);
@@ -97,6 +118,26 @@ public class WebSocket {
         }
     }
 
+    public void sendSoundFile(String userId, String filePath){
+        Session session = sessionPool.get(userId);
+        if (session != null && session.isOpen()) {
+            synchronized (session){
+                RemoteEndpoint.Basic endpoint = session.getBasicRemote();
+                try {
+                    byte[] data = new byte[1024];
+                    FileInputStream inputStream = new FileInputStream(filePath);
+                    while(inputStream.read(data) != -1){
+                        endpoint.sendBinary(ByteBuffer.wrap(data));
+                        data = new byte[1024];
+                    }
+                    inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @RequestMapping("/time")
     public static JSONObject getDate() {
         Calendar calendar = new GregorianCalendar();
@@ -111,9 +152,9 @@ public class WebSocket {
 
     Call getOkhttpCall(String endpoint, JSONObject jsonObject, String key){
         OkHttpClient client = new OkHttpClient().newBuilder()
-                .connectTimeout(Duration.ofSeconds(5))
-                .writeTimeout(Duration.ofSeconds(5))
-                .readTimeout(Duration.ofSeconds(5))
+                .connectTimeout(Duration.ofSeconds(60))
+                .writeTimeout(Duration.ofSeconds(60))
+                .readTimeout(Duration.ofSeconds(60))
                 .build();
         Request request = new Request.Builder()
                 .url(endpoint)
@@ -126,7 +167,7 @@ public class WebSocket {
     }
 
     void callApiOld(JSONObject jsonObject, String key){
-        Call call = getOkhttpCall("https://api.openai.com/v1/completions",
+        Call call = getOkhttpCall(GPT_3_URL,
                 jsonObject, key);
         call.enqueue(new Callback() {
             @Override
@@ -151,30 +192,28 @@ public class WebSocket {
                         }
                         reader.close();
                         // End Message
-                        System.out.println("To" + userId + " :\n" + res + "\n");
+                        System.out.println(getDate().getString("time") + "\nTo" + userId + " :\n" + res + "\n");
                     }catch (IOException e) {
                         sendOneMessage(userId, e.getMessage());
                         e.printStackTrace();
                     }
-                    sendOneMessage(userId, "///**1100101**///");
+                    sendOneMessage(userId, SEND_END);
                 } else {
                     sendOneMessage(userId, "Error while calling OpenAI's API.\n");
-                    sendOneMessage(userId, response.message());
-                    sendOneMessage(userId, response.code() + response.body().string());
-                    sendOneMessage(userId, "///**1100101**///");
+                    sendOneMessage(userId, "Code: " + response.code() + "\n" + response.body().string());
+                    sendOneMessage(userId, SEND_END);
                 }
             }
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                sendOneMessage(userId, "Failed to call OpenAI's API. MSG:\n");
-                sendOneMessage(userId, e.getMessage());
-                sendOneMessage(userId, "///**1100101**///");
+                sendOneMessage(userId, "Failed to call OpenAI's API. Plz try again later:\n");
+                sendOneMessage(userId, SEND_END);
             }
         });
 
     }
     void callApiNew(JSONObject jsonObject, String key){
-        Call call = getOkhttpCall("https://api.openai.com/v1/chat/completions",
+        Call call = getOkhttpCall(GPT_3_5_URL,
                 jsonObject, key);
         call.enqueue(new Callback() {
             @Override
@@ -210,26 +249,63 @@ public class WebSocket {
                         reader.close();
                         inputStream.close();
                         // End Message
-                        System.out.println("To" + userId + " :\n" + res + "\n");
+                        System.out.println(getDate().getString("time") + "\nTo" + userId + " :\n" + res + "\n");
                     }catch (IOException e) {
                         sendOneMessage(userId, e.getMessage());
                         e.printStackTrace();
                     }
-                    sendOneMessage(userId, "///**1100101**///");
+                    sendOneMessage(userId, SEND_END);
                 } else {
                     sendOneMessage(userId, "Error while calling OpenAI's API.\n");
-                    sendOneMessage(userId, response.message());
                     sendOneMessage(userId, "Code: " + response.code() + "\n" + response.body().string());
-                    sendOneMessage(userId, "///**1100101**///");
+                    sendOneMessage(userId, SEND_END);
                 }
             }
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 sendOneMessage(userId, "Failed to call OpenAI's API. Plz try again later:\n");
-                sendOneMessage(userId, "///**1100101**///");
+                sendOneMessage(userId, SEND_END);
             }
         });
 
+    }
+
+    void callTtsApi(JSONObject jsonObject){
+        getOkhttpCall(LOCAL_AUDIO_API_URL, jsonObject, "").enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                sendOneMessage(userId, "Failed to generate sound file.");
+                sendOneMessage(userId, FILE_ERROR);
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) {
+                if (response.isSuccessful()) {
+                    InputStream inputStream = response.body().byteStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    StringBuilder res = new StringBuilder();
+                    try {
+                        while ((line = reader.readLine())!=null){
+                            res.append(line);
+                        }
+                        reader.close();
+                        inputStream.close();
+                        sendSoundFile(userId, res.toString());
+                        // End Message
+                        System.out.println(getDate().getString("time") + "\nTTS To: " +
+                                userId + " :\n" + res + "\n");
+                    }catch (IOException e) {
+                        sendOneMessage(userId, e.getMessage());
+                        e.printStackTrace();
+                    }
+                    sendOneMessage(userId, FILE_END);
+                } else {
+                    sendOneMessage(userId, FILE_ERROR);
+                    System.out.println("Error Code : " + response.code());
+                }
+            }
+        });
     }
 
     /*    public void sendAllMessage(String message) {
